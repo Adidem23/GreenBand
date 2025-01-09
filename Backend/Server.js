98808e75a1da39d1bc865690d274bea9da1c04f6
Adidem23 <adityasuryawanshi5451@gmail.com>
2025-01-09 17:23:20 +0530

    Feat:Added cloud MongoStorage

--- Backend/Server.js
+++ Backend/Server.js
@@ -5,7 +5,8 @@ const CORS = require('cors')
 const bodyParser = require('body-parser');
 const GreenBandRoutes = require('./Routes/index')
 const mongoose = require('mongoose');
-const path=require('path');
+const path = require('path');
+require('dotenv').config();
 
 app.use(express.json())
 app.use(bodyParser.urlencoded({ extended: true }))
@@ -19,12 +20,20 @@ app.use(CORS({
 app.use('/api', GreenBandRoutes);
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
+const DatabasePassword = process.env.DATABASEUSERPASSWORD
+
 // Local MongoDB Storage
-mongoose.connect("mongodb://0.0.0.0:27017/GreenBandUsers")
-.then(() => console.log('MongoDB connected')).catch((err) => console.log(err));
+// mongoose.connect("mongodb://0.0.0.0:27017/GreenBandUsers")
+// .then(() => console.log('MongoDB connected')).catch((err) => console.log(err));
 
 // Global MongoDB Storage
+const URI = `mongodb+srv://adityavanshi5451:${DatabasePassword}@greenbandusers.o5ssg.mongodb.net/?retryWrites=true&w=majority&appName=GreenBandUsers`
 
+mongoose.connect(URI).then(() => {
+    console.log("Connected To MongoDBCompass")
+}).catch((err) => {
+    console.log(`Error While Connecting Database : ${err}`)
+})
 
 app.listen(PORT, () => {
     console.log(`App is Running on PORT : ${PORT}`)
