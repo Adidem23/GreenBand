1b95e4534478e24de55f34f1d8467d1d7db7dd07
Adidem23 <adityasuryawanshi5451@gmail.com>
2025-01-09 22:39:27 +0530

    Feat:Added Some path changes in multer

--- Backend/Routes/index.js
+++ Backend/Routes/index.js
@@ -2,11 +2,12 @@ const express = require('express');
 const multer = require('multer');
 const Router = express.Router();
 const GreenBandController = require('../Controllers');
+const path = require('path');
 
 
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
-        cb(null, 'uploads/');
+        cb(null, path.join(__dirname, 'uploads'));
     },
     filename: (req, file, cb) => {
         cb(null, `${Date.now()}-${file.originalname}`);
